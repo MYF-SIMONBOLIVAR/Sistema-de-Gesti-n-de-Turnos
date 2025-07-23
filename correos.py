a8c4ce8be313694f63b43a022efd906e0648fe12
MUELLES Y FRENOS SIMON BOLIVAR <tic3@repuestossimonbolivar.com>
2025-07-23 13:47:51 -0500

    Add files via upload

---
+++ correos.py
@@ -0,0 +1,12 @@
+# Diccionario de correos por área
+CORREOS_JEFES = {
+    "Logistica": "coordinadorlogistico@repuestossimonbolivar.com",
+    "Compras": "coordinadorcompras@repuestossimonbolivar.com",
+    "Cartera": "coordinadorcartera@repuestossimonbolivar.com",
+    "Contable": "coordinadorcontable@repuestossimonbolivar.com",
+    "Despachos": "despachos@repuestossimonbolivar.com",
+    "Juridica": "juridica@empresa.com",
+    "Gestion Humana": "directoradministrativo@repuestossimonbolivar.com",
+    "SST": "sst@empresa.com",
+    "TI": "tic2@repuestossimonbolivar.com"
+}
